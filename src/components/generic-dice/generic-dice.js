0d51a665271e6cc48d19a03734d15b3ac309f068
Nadjib Amar <hello@ebabel.eu>
2016-08-14 15:18:52 +0200

    animation

--- src/components/generic-dice/generic-dice.js
+++ src/components/generic-dice/generic-dice.js
@@ -5,7 +5,55 @@ import DiceShadow from '../dice-shadow/dice-shadow';
 import './generic-dice.scss';
 
 export default class GenericDice extends Component {
+  componentWillMount() {
+    this.setState({
+      face: this.props.face,
+      rolling: this.props.rolling,
+      animation: '',
+    });
+  }
+
+  componentDidMount() {
+    if (this.state.rolling) {
+      this.roll();
+    }
+  }
+
+  // @animation: string CSS class parameter that can be 'throw1', 'throw2', 'throw3' or 'throw4'.
+  // @duration: length of time in milliseconds during which the animation should last.
+  roll(animation = 'thrown', duration = 2000) {
+    let start;
+    let animationId = null;
+
+    const rolling = timestamp => {
+      if (!start) {
+        start = timestamp;
+        this.setState({
+          rolling: true,
+          animation,
+        });
+      }
+
+      const delta = timestamp - start;
+
+      if (delta > duration) {
+        start = null;
+        window.cancelAnimationFrame(animationId);
+        this.setState({
+          rolling: false,
+          animation: '',
+        });
+      } else {
+        window.requestAnimationFrame(rolling);
+      }
+    };
+
+    animationId = window.requestAnimationFrame(rolling);
+  }
+
   render() {
+    const diceClass = `dice ${this.state.animation || ''}`;
+
     return (
       <div
         className="dice-wrapper"
@@ -13,8 +61,11 @@ export default class GenericDice extends Component {
           { fontSize: `${this.props.size}px` }
         }
       >
-        <div className="dice">
-          <DiceFace face={this.props.face} />
+        <div
+          className={diceClass}
+          ref="dice"
+        >
+          <DiceFace face={this.state.face} />
         </div>
         <DiceShadow />
       </div>
--- src/index.js
+++ src/index.js
@@ -7,15 +7,13 @@ import './index.scss';
 render((
   <div>
     <Dice />
-    <Dice face={5} />
-    <Dice face={6} rolling={false} size={5} />
-    <Dice face={4} rolling={false} size={10} />
-    <Dice face={3} rolling={false} size={20} />
-    <Dice face={2} rolling={false} size={40} />
-    <Dice face={1} rolling={false} size={80} />
+    <Dice face={6} />
+    <Dice face={5} rolling={false} size={5} />
+    <Dice face={4} rolling={false} size={7} />
+    <Dice face={3} rolling={false} size={10} />
+    <Dice face={2} rolling={false} size={12} />
   </div>
 ), document.getElementById('root'));
-
 
 function main() {
   const app = {};
