f950e4b88e2f6d87c6ae685fd203b04838b607c4
Nadjib Amar <hello@ebabel.eu>
2016-08-16 23:52:40 +0200

    writing first unit test in progress - not working yet

---
+++ components/generic-dice/__tests__/generic-dice.react-test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Dice from '../generic-dice.js';
+
+describe('GenericDice', () => {
+  it('never fails', () => {
+    expect(true).toEqual(true);
+  });
+});
