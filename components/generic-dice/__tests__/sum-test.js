08d825967e0a7004fde6c86797b80a3752f95f1b
Nadjib Amar <hello@ebabel.eu>
2016-08-20 17:58:39 +0200

    basic test is passing

--- components/generic-dice/__tests__/sum-test.js
+++ components/generic-dice/__tests__/sum-test.js
@@ -1,8 +1,6 @@
-jest.unmock('../sum');
-
 describe('sum', () => {
   it('adds 1 + 2 to equal 3', () => {
-    const sum = require('../sum');
+    const sum = require('../sum.js');
     expect(sum(1, 2)).toBe(3);
   });
 });
