5a5220e37a9d6e40f276f69251e269bc2ef1c2da
Nadjib Amar <hello@ebabel.eu>
2016-08-21 00:25:35 +0200

    thresholds

--- components/dice-shadow/__tests__/dice-shadow-test.js
+++ components/dice-shadow/__tests__/dice-shadow-test.js
@@ -6,4 +6,11 @@ describe('dice-shadow', () => {
   it('exists', () => {
     expect(DiceShadow).toBeDefined();
   });
+
+  it('renders an element', () => {
+    const diceShadow = new DiceShadow();
+    const result = diceShadow.render.mockReturnValueOnce();
+    console.log(result);
+    expect(result).toBeDefined();
+  });
 });
