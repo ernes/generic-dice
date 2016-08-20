8e371969dcb9ab10da1028f808332f4d02616758
Nadjib Amar <hello@ebabel.eu>
2016-08-20 18:48:31 +0200

    vanilla tests are passing but my own code is failing to be tested

---
+++ components/generic-dice/__tests__/generic-dice-test.js
@@ -0,0 +1,8 @@
+describe('generic-dice', () => {
+  it('exists', () => {
+    const GenericDice = require('../generic-dice.js');
+    console.log(GenericDice);
+
+    expect(GenericDice).toBeDefined();
+  });
+});
--- components/generic-dice/__tests__/sum-test.js
+++ components/generic-dice/__tests__/sum-test.js
@@ -1,6 +1,11 @@
+import sum from '../sum';
+
 describe('sum', () => {
-  it('adds 1 + 2 to equal 3', () => {
-    const sum = require('../sum.js');
-    expect(sum(1, 2)).toBe(3);
+  it('adds 1 + 2 to equal 3', sum => {
+    expect(sum(1, 2)).toEqual(3);
+  });
+
+  it('add 3 + 6 to equal 9', sum => {
+    expect(sum(3, 6)).toEqual(9);
   });
 });
