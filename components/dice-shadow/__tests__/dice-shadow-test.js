e862817824da09782295777cdd507384d65f642a
Nadjib Amar <hello@ebabel.eu>
2016-08-21 23:01:29 +0200

    increased test coverage of dice shadow

--- components/dice-shadow/__tests__/dice-shadow-test.js
+++ components/dice-shadow/__tests__/dice-shadow-test.js
@@ -3,20 +3,30 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-const DiceShadow = require('../dice-shadow').default;
+jest.unmock('../dice-shadow');
 
 describe('DiceShadow', () => {
   it('exists.', () => {
+    const DiceShadow = require('../dice-shadow').default;
     expect(DiceShadow).toBeDefined();
   });
 
   it('renders an element that is not animated.', () => {
+    const DiceShadow = require('../dice-shadow').default;
     const component = renderer.create(
       <DiceShadow animation="" />
     );
     const tree = component.toJSON();
 
-    console.log(`The tree is ${tree}`);
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders an element that *is* animated.', () => {
+    const DiceShadow = require('../dice-shadow').default;
+    const component = renderer.create(
+      <DiceShadow animation="thrown" />
+    );
+    const tree = component.toJSON();
 
     expect(tree).toMatchSnapshot();
   });
