ee52d0c7571e0f9bac8ffa994be8338d628d9320
Nadjib Amar <hello@ebabel.eu>
2016-09-03 15:42:15 +0200

    in progress

---
+++ src/vanilla-js/__tests__/generic-dice-test.js
@@ -0,0 +1,29 @@
+/* global describe jest it expect */
+
+import GenericDice from '../generic-dice';
+
+jest.unmock('../generic-dice');
+
+describe('GenericDice', () => {
+  it('exists.', () => {
+    expect(GenericDice).toBeDefined();
+  });
+
+  it('has a default face property set to 1', () => {
+    const dice = new GenericDice();
+
+    expect(dice.face).toBe(1);
+  });
+
+  it('has a default rolling property set to true', () => {
+    const dice = new GenericDice();
+
+    expect(dice.rolling).toBe(true);
+  });
+
+  it('has a default animation property set to an empty string', () => {
+    const dice = new GenericDice();
+
+    expect(dice.animation).toBe('');
+  });
+});
---
+++ src/vanilla-js/generic-dice.js
@@ -0,0 +1,7 @@
+export default class GenericDice {/* istanbul ignore next */
+  constructor() {
+    this.face = 1;
+    this.rolling = true;
+    this.animation = '';
+  }
+}
