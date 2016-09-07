f31d3f12876814296a8f21e9d584f3fd4ce79cac
Nadjib Amar <hello@ebabel.eu>
2016-09-08 00:54:15 +0200

    Revert "simplified the dice animation by removing requestAnimationFrame"
    This reverts commit 79ed820ae8172204360dfce3263a7ece624d15c7.

--- src/react/generic-dice/roll.js
+++ src/react/generic-dice/roll.js
@@ -1,13 +1,31 @@
-export function roll() {
-  this.setState({
-    rolling: true,
-    animation: 'thrown',
-  });
+// @animation: string CSS class parameter that can be 'thrown' or ''.
+// @duration: length of time in milliseconds during which the animation should last.
+export function roll(animation = 'thrown', duration = 2000) {
+  let start;
+  let animationId = null;
 
-  setTimeout(() => {
-    this.setState({
-      rolling: false,
-      animation: '',
-    });
-  }, 700);
+  const rolling = timestamp => {
+    if (!start) {
+      start = timestamp;
+      this.setState({
+        rolling: true,
+        animation,
+      });
+    }
+
+    const delta = timestamp - start;
+
+    if (delta > duration) {
+      start = null;
+      window.cancelAnimationFrame(animationId);
+      this.setState({
+        rolling: false,
+        animation: '',
+      });
+    } else {
+      window.requestAnimationFrame(rolling);
+    }
+  };
+
+  animationId = window.requestAnimationFrame(rolling);
 }
