d81012c1bc1a5b5b1c3b6cc7cd0245bda9484104
Nadjib Amar <hello@ebabel.eu>
2016-09-14 16:16:49 +0200

    added a file for constants, the first one being the duration

--- src/react/generic-dice/roll.js
+++ src/react/generic-dice/roll.js
@@ -1,9 +1,11 @@
+import * as C from './constants';
+
 export let resetAnimation;
 
 export function roll(e) {
   // todo: do something with this data.
   // todo: consider getting that 700 figure from constants.
-  const duration = e && e.detail && e.detail.duration || 700;
+  const duration = e && e.detail && e.detail.duration || C.DURATION;
 
   resetAnimation = () => {
     this.setState({
