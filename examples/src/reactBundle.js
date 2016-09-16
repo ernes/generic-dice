c7384f5b398e232dee94f38d9de9e25676273a02
Nadjib Amar <hello@ebabel.eu>
2016-09-16 05:42:08 +0200

    react examples

--- examples/src/reactBundle.js
+++ examples/src/reactBundle.js
@@ -12,23 +12,23 @@ render((
     <Dice />
 
     <h3>Dice with custom face, size and rolling</h3>
-    <code>&lt;Dice face={6} size={14} rolling={false} /&gt;</code>
+    <code>&lt;Dice face=&#123;6&#125; size=&#123;14&#125; rolling=&#123;false&#125; /&gt;</code>
     <Dice face={6} size={14} rolling={false} />
 
     <h3>Dice with custom face and size</h3>
-    <code>&lt;Dice face={5} size={10} /&gt;</code>
+    <code>&lt;Dice face=&#123;5&#125; size=&#123;10&#125; /&gt;</code>
     <Dice face={5} size={10} />
 
     <h3>Dice with custom face and size</h3>
-    <code>&lt;Dice face={4} size={10} /&gt;</code>
+    <code>&lt;Dice face=&#123;4&#125; size=&#123;10&#125; /&gt;</code>
     <Dice face={4} size={10} />
 
     <h3>Dice with custom face and size and shortest duration</h3>
-    <code>&lt;Dice face={3} size={10} duration={500} /&gt;</code>
+    <code>&lt;Dice face=&#123;3&#125; size=&#123;10&#125; duration=&#123;500&#125; /&gt;</code>
     <Dice face={3} size={10} duration={500} />
 
     <h3>Dice with custom face and size and longest duration</h3>
-    <code>&lt;Dice face={2} size={10} duration={900} /&gt;</code>
+    <code>&lt;Dice face=&#123;2&#125; size=&#123;10&#125; duration=&#123;900&#125; /&gt;</code>
     <Dice face={2} size={10} duration={900} />
   </div>
 ), document.getElementById('react-game'));
